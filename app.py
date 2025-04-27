3aa3d6f62424358a23e448bccfb3b53ef61aa700
crossmarkv <mg3137452@gmail.com>
2025-04-27 17:42:02 -0500

    Primer commit para mi app Flask en Azure

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for  # type: ignore
+
+app = Flask(__name__)
+
+# Credenciales
+USUARIO = "teamo"
+CLAVE = "1234"
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def do_login():
+    usuario = request.form.get("usuario")
+    clave = request.form.get("clave")
+    if usuario == USUARIO and clave == CLAVE:
+        return redirect(url_for("carta"))
+    else:
+        return "<h2 style='color:red;'>Usuario o contraseña incorrectos. <a href='/'>Volver</a></h2>"
+
+@app.route("/carta")
+def carta():
+    return render_template("carta.html")
+
+# Este bloque solo se usa al correr localmente
+if __name__ == "__main__":
+    app.run()
